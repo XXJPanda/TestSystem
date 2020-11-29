62222ddf6e1c0baa0175096b44d5452464e7b43d
XXJPanda <624035709@qq.com>
2020-11-29 14:43:00 +0800

    第一次实验

---
+++ src/com/Service/UserService.java
@@ -0,0 +1,17 @@
+package com.Service;
+
+import com.Dao.UserDao;
+import com.Dao.impl.UserDaoImpl;
+import com.Entity.User;
+
+public class UserService {
+    private UserDao userDao=new UserDaoImpl();
+
+    public User queryUser(String username, String password) {
+        return userDao.queryUser(username,password);
+    }
+
+    public void insert(User user) {
+         userDao.insert(user);
+    }
+}
